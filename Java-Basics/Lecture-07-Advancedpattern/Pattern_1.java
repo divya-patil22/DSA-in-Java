537d51657dfce31db8bc5d00d57d1da4a856479f
Divya patil <divya22sonawane@gmail.com>
2026-08-17 18:16:03 +0530

    Lecture-07-Added

---
+++ Java-Basics/Lecture-07-Advancedpattern/Pattern_1.java
@@ -0,0 +1,43 @@
+
+
+public class Pattern_1 {
+     public static void main(String[] args) {
+         int n = 5;
+         int m = 5;
+
+         //Upper part
+         for (int i = 1; i <= n; i++) {
+              //first part
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("* ");
+             }
+              //space 
+             int space = 2*(n-i);
+             for (int j = 1; j <= space; j++) {
+                
+              System.out.print("  "); 
+             }
+             //second part
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+         //Lower part
+          for (int i = n; i >= 1; i--) {
+             
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("* ");
+             }
+
+             int space = 2*(n-i);
+             for (int j = 1; j <= space; j++) {
+                System.out.print("  "); 
+             }
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+     }         
+}
