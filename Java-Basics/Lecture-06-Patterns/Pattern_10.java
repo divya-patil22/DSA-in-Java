0b75a517923c58d9867e74a74cab9673b052e828
Divya patil <divya22sonawane@gmail.com>
2026-08-17 18:12:15 +0530

    Lecture-06-Added

---
+++ Java-Basics/Lecture-06-Patterns/Pattern_10.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+
+public class Pattern_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of Rows: ");
+        int n = sc.nextInt();
+        System.out.println("Enter Number of Columns: ");
+        int m = sc.nextInt();
+
+        for(int i = 1; i<=n; i++){
+              for(int j = 1; j <= m; j++){
+                            System.out.print("* ");
+              }
+              System.out.println();
+        }
+    }          
+}
